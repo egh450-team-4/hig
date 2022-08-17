819a4484e56299d85b6c99540cff06c4893ba9d9
Lorenzo <lorenzo@nioel.com>
2022-08-17 19:36:30 +1000

    remove disros definition

--- init_onboard.sh
+++ init_onboard.sh
@@ -3,16 +3,6 @@
 # on the onboard computer at startup
 cd ~/hig && git fetch --all && git reset --hard origin/master
 cd ~
-disros() {
-  # Setup for distributed ROS
-  export ROS_IP="$(hostname -I | cut -d' ' -f1)"
-  echo "Identifying as: $ROS_IP"
-  if [ "$#" -eq 1 ]
-  then
-    export ROS_MASTER_URI="http://$1:11311"
-    echo "Connecting to: $ROS_MASTER_URI"
-  fi
-}
 disros
 sleep 1;
 roscore &
