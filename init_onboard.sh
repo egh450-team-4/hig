294d2cad4437d5ca3ac47b3b02ece45207564944
Lorenzo <lorenzo@nioel.com>
2022-08-17 19:42:29 +1000

    add logging

--- init_onboard.sh
+++ init_onboard.sh
@@ -1,15 +1,22 @@
 #!/bin/bash -i
 # This script should be executed
 # on the onboard computer at startup
-cd ~/hig && git fetch --all && git reset --hard origin/master
-cd ~
+cd ~/hig && git fetch --all && git reset --hard origin/master;
+cd ~;
+echo "1";
 echo "PATH IS $PATH";
+echo $PATH;
+echo "2";
 disros
 sleep 1;
+echo "[onboard] running roscore"
 roscore &
 sleep 10;
+echo "[onboard] running roslaunch qutas_lab_450";
 roslaunch qutas_lab_450 environment.launch &
 sleep 10;
+echo "[onboard] running catkin's control.launch";
 roslaunch ~/catkin_ws/launch/control.launch &
 sleep 1;
+echo "[onboard] running depthai_publisher";
 rosrun depthai_publisher dai_publisher &
